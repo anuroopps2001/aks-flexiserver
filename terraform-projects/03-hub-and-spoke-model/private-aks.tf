ed187dffd395b0ab161128276cc771580f6f1aff
Anuroop P S <anuroopps2001@gmail.com>
2026-04-24 10:40:28 +0530

    added nsg rules for aks subnet to prevent access from all other vnets[skip ci]

--- terraform-projects/03-hub-and-spoke-model/private-aks.tf
+++ terraform-projects/03-hub-and-spoke-model/private-aks.tf
@@ -155,7 +155,7 @@ resource "azurerm_network_security_rule" "deny_envoy_access" {
   protocol = "Tcp"
   source_port_range = "*"
   destination_port_ranges = [ "80", "443" ]
-  source_address_prefix = "*"
+  source_address_prefix = "10.0.3.0/24"
   destination_address_prefix = "10.1.1.6"
   resource_group_name = azurerm_resource_group.rgs["rg-spoke-workloads"].name
   network_security_group_name = azurerm_network_security_group.aks_custom_nsg.name
